7d24ace5f8ffdfadb2bf9188daa9c7746be877f6
utsav747 <utsavsaini747@gmail.com>
2017-05-12 10:46:43 +0530

    corrected syntax errors and added props to the components

--- app/index.js
+++ app/index.js
@@ -24,97 +24,36 @@ class Sidebar extends React.Component {
   }
 }
 
-// class Button extends React.Component {
-//   render() {
-//     return(
-//       <div className = "button">
-//       </div>
-//     )
-//   }
-// }
-
-// class IconView extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <Profile photo = "photo-url-here" />
-//         <Icons />
-//         <Footer />
-//       </div>
-//     )
-//   }
-// }
-
-// class Icons extends React.Component {
-//   render() {
-//     var icons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//     return (
-//       <div>
-//         <ul>
-//           {this.props.icons.map(function(icon){
-//             <li>icon</li>
-//           })}
-//         </ul>
-//       </div>
-//     )
-//   }
-// }
-
-// class Profile extends React.Component {
-//   render() {
-//     var profilepic =
-//     return(
-//       <div className = "picContainer">
-//         {this.props.photo}
-//       </div>
-//     )
-//   }
-// }
-
-// class Footer extends React.Component{
-//   render() {
-//     var footerIcons = [footericon1, footericon2, footericon3];
-//     return(
-//       <div>
-//         {this.props.footerIcons.map(function(icon){
-//           <li>icon</li>
-//         })}
-//       </div>
-//     )
-//   }
-// }
-
-class DetailView extends React.Component {
+class Button extends React.Component {
   render() {
     return(
-      <div>
-        // <ProfileDetails />
-        <Menu />
-        // <FooterOptions />
+      <div className = "button">
       </div>
     )
   }
 }
 
-// class ProfileDetails extends React.Component{
-//   render() {
-//     return(
-//       <div>
-//         <li>Name</li>
-//         <li>Email</li>
-//       </div>
-//     )
-//   }
-// }
-
-class Menu extends React.Component{
+class IconView extends React.Component {
   render() {
-    return(
-      Var menus = ["Contacts", "Product", "Sales", "Purchase", "Shipping", "Inventory & Stock", "Production", "Financial", "Settings", "Administration"];
+    var icons = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+    var footerIcons = ["footericon1", "footericon2", "footericon3"];
+    return (
+      <div>
+        <Profile photo = "photo-url-here" />
+        <Icons items = {icons}/>
+        <Footer items = {footerIcons}/>
+      </div>
+    )
+  }
+}
+
+class Icons extends React.Component {
+  render() {
+    return (
       <div>
         <ul>
-          {this.props.menus.map(function(item){
-            <li>item</li>
+          {this.props.items.map(function(icon, index){
+            return <li key = {index}>icon</li>;
           })}
         </ul>
       </div>
@@ -122,34 +61,94 @@ class Menu extends React.Component{
   }
 }
 
-// class FooterOptions extends React.Component{
-//   render() {
-//     var footerOptions = [footerOption1, footerOption2, footerOption3];
-//     return(
-//       <div>
-//         {this.props.footerOptions.map(function(option){
-//           <li>option</li>
-//         })}
-//       </div>
-//     )
-//   }
-// }
+class Profile extends React.Component {
+  render() {
+    return(
+      <div className = "picContainer">
+        {this.props.photo}
+      </div>
+    )
+  }
+}
 
-// class SubMenu extends React.component{
-//   render() {
-//     var submenuOptions = ["Product templates", "Products", "Categories", "Attributes", "Tree Nodes", "Gift Card", "Bill of Materials(BOM)", "Price Lists", "Product suppliers", "Attribute sets", "Reports", "Import product locations"];
-//     return(
-//       <div>
-//         <h3 className = "SubMenuHeading">{this.props.SuperName}</h3>
-//         <ul>
-//           {this.props.submenuOptions.map(function(item){
-//             <li>item</li>
-//           })}
-//         </ul>
-//       </div>
-//     )
-//   }
-// }
+class Footer extends React.Component{
+  render() {
+    return(
+      <div>
+        {this.props.items.map(function(footericon, index){
+          <li key = {index}>footericon</li>
+        })}
+      </div>
+    )
+  }
+}
+
+class DetailView extends React.Component {
+  render() {
+    var menus = ["Contacts", "Product", "Sales", "Purchase", "Shipping", "Inventory & Stock", "Production", "Financial", "Settings", "Administration"];
+    var footerOptions = ["footerOption1", "footerOption2", "footerOption3"];
+    return(
+      <div>
+        <ProfileDetails />
+        <Menu items = {menus}/>
+        <FooterOptions items = {footerOptions} />
+      </div>
+    )
+  }
+}
+
+class ProfileDetails extends React.Component{
+  render() {
+    return(
+      <div>
+        <li>Name</li>
+        <li>Email</li>
+      </div>
+    )
+  }
+}
+
+class Menu extends React.Component{
+  render() {
+    return(
+      <div>
+        <ul>
+          {this.props.items.map(function(item){
+            return <li key = {item}>{item}</li>;
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+class FooterOptions extends React.Component{
+  render() {
+    return(
+      <div>
+        {this.props.items.map(function(option){
+          <li key = {option}>option</li>
+        })}
+      </div>
+    )
+  }
+}
+
+class SubMenu extends React.Component{
+  render() {
+    var submenuOptions = ["Product templates", "Products", "Categories", "Attributes", "Tree Nodes", "Gift Card", "Bill of Materials(BOM)", "Price Lists", "Product suppliers", "Attribute sets", "Reports", "Import product locations"];
+    return(
+      <div>
+        <h3 className = "SubMenuHeading">{this.props.SuperName}</h3>
+        <ul>
+          {this.props.submenuOptions.map(function(item){
+            <li key = {item}>item</li>
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <App />,
